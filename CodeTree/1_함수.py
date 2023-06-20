a813cf12656a5734f47d3710cbf53b747d8bda39
Miensoap <miensop51@gmail.com>
2023-06-21 05:50:09 +0900

    2230621_05:49

---
+++ CodeTree/1_함수.py
@@ -0,0 +1,10 @@
+#함수정의
+# def print_10_stars():
+#     print('*'*10)
+# for i in range(5):
+#     print_10_stars()
+
+def print_n_lines(n):
+    for i in range(n):
+        print('12345^&*()_')
+print_n_lines(int(input()))
